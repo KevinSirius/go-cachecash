f5333a7135f2a2e259e59ac1e8040bd0d174df65
KevinSirius <kevinsiriusyang@icloud.com>
2020-10-24 14:22:58 -0400

    travisCI fix

--- ci/setup.sh
+++ ci/setup.sh
@@ -2,7 +2,7 @@
 set -xe
 
 update_docker_compose() {
-  DOCKER_COMPOSE_VERSION=1.24.0
+  DOCKER_COMPOSE_VERSION=1.27.4
 
   sudo rm /usr/local/bin/docker-compose
   curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
@@ -11,9 +11,9 @@ update_docker_compose() {
 }
 
 start_db() {
-  while ! docker run --rm --net=cachecash postgres:11 psql 'host=ledger-db port=5432 user=postgres dbname=ledger sslmode=disable' -c 'select 1;'; do sleep 10; done
-  while ! docker run --rm --net=cachecash postgres:11 psql 'host=publisher-db port=5432 user=postgres dbname=publisher sslmode=disable' -c 'select 1;'; do sleep 10; done
-  while ! docker run --rm --net=cachecash postgres:11 psql 'host=kvstore-test port=5432 user=postgres dbname=kvstore sslmode=disable' -c 'select 1;'; do sleep 10; done
+  while ! docker run --rm --net=cachecash --env-file ./deploy/secrets/ledger.secret postgres:11; do sleep 10; done
+  while ! docker run --rm --net=cachecash --env-file ./deploy/secrets/publisher.secret postgres:11postgres:11; do sleep 10; done
+  while ! docker run --rm --net=cachecash --env-file ./deploy/secrets/logpiped.secret postgres:11postgres:11; do sleep 10; done
 }
 
 # check the build image's dockerfile; if it's been altered, build it.
@@ -34,9 +34,9 @@ fi
 case "$BUILD_MODE" in
   test)
     docker network create cachecash --opt com.docker.network.bridge.enable_ip_masquerade=false || true
-    time docker run -d -p 5433:5432 -e POSTGRES_DB=ledger --name ledger-db --net=cachecash postgres:11
-    time docker run -d -p 5434:5432 -e POSTGRES_DB=publisher --name publisher-db --net=cachecash postgres:11
-    time docker run -d -p 5435:5432 -e POSTGRES_DB=kvstore --name kvstore-test --net=cachecash postgres:11
+    time docker run -d -p 5433:5432 --net=cachecash --env-file ./deploy/secrets/ledger.secret postgres:11
+    time docker run -d -p 5434:5432 --net=cachecash --env-file ./deploy/secrets/publisher.secret postgres:11
+    time docker run -d -p 5435:5432 --net=cachecash --env-file ./deploy/secrets/logpiped.secret postgres:11
     time docker build -t cachecash-ci ci
 
     # wait until the databases are up
